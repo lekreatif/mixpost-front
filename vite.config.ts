61b9678cf52f4d9da618a2cc24501e1dbbbd232d
gederooney <gyebadokpo61@gmail.com>
2024-09-24 16:30:31 -0400

    vite_config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ _, mode }) => {
+export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [react()],
